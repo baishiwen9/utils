7fef30d117a2688a8cc0b96eaaef85ed2fec7c5d
baishiwen <baishiwen@babytree-inc.com>
2019-11-26 19:46:36 +0800

    feat 完成version模块

--- dist/utils.js
+++ dist/utils.js
@@ -1846,7 +1846,8 @@ __webpack_require__.r(__webpack_exports__);
  * 比较大
  */
 function greater(v1, v2) {
-  if (v1 > v2) {
+  if (compare(v1, v2) > 0) {
+    console.log('v1 > v2');
     return true;
   }
 
@@ -1858,7 +1859,8 @@ function greater(v1, v2) {
 
 
 function smaller(v1, v2) {
-  if (v1 < v2) {
+  if (compare(v1, v2) < 0) {
+    console.log('v1 < v2');
     return true;
   }
 
@@ -1870,11 +1872,36 @@ function smaller(v1, v2) {
 
 
 function equal(v1, v2) {
-  if (v1 === v2) {
+  if (compare(v1, v2) === 0) {
+    console.log('v1 === v2');
     return true;
   }
 
   return false;
+}
+
+function compare(v1, v2) {
+  var vArr1 = v1 && v1.replace(/[A-Za-z]/, '').split('.');
+  var vArr2 = v2 && v2.replace(/[A-Za-z]/, '').split('.');
+  var $length = Math.max(vArr2.length, vArr1.length);
+  var vi1 = 0;
+  var vi2 = 0;
+  var i = 0;
+
+  for (i = 0; i < $length; i += 1) {
+    vi1 = Number(vArr1[i]) ? Number(vArr1[i]) : 0;
+    vi2 = Number(vArr2[i]) ? Number(vArr2[i]) : 0;
+
+    if (vi2 > vi1) {
+      return 1;
+    }
+
+    if (vi2 < vi1) {
+      return -1;
+    }
+  }
+
+  return 0;
 }
 
 /* harmony default export */ __webpack_exports__["default"] = ({
--- src/version.js
+++ src/version.js
@@ -8,8 +8,9 @@
  * 比较大
  */
  function greater(v1, v2) {
-    if (v1 > v2) {
-        return true;
+    if (compare(v1, v2) > 0) {
+      console.log('v1 > v2');
+      return true;
     }
     return false;
  }
@@ -18,22 +19,45 @@
   * 比较小
   */
  function smaller(v1, v2) {
-    if (v1 < v2) {
-        return true;
-    }
-    return false;
+      if (compare(v1, v2) < 0) {
+         console.log('v1 < v2');
+         return true;
+      }
+      return false;
  }
 
  /**
   * 相等
   */
  function equal(v1, v2) {
-    if (v1 === v2) {
-        return true;
-    }
-    return false;
+      if (compare(v1, v2) === 0) {
+         console.log('v1 === v2');
+         return true;
+      }
+      return false;
  }
 
+
+
+function compare(v1, v2) {
+   const vArr1 = v1 && v1.replace(/[A-Za-z]/, '').split('.');
+   const vArr2 = v2 && v2.replace(/[A-Za-z]/, '').split('.');
+   const $length = Math.max(vArr2.length, vArr1.length);
+   let vi1 = 0;
+   let vi2 = 0;
+   let i = 0;
+   for (i = 0; i < $length; i += 1) {
+      vi1 = Number(vArr1[i]) ? Number(vArr1[i]) : 0;
+      vi2 = Number(vArr2[i]) ? Number(vArr2[i]) : 0;
+      if (vi2 > vi1) {
+         return 1;
+      }
+      if (vi2 < vi1) {
+         return -1;
+      }
+   }
+   return 0;
+}
 
  
  export default{
